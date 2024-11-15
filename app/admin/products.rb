f615a6e8a988a4a678d386dab6a404c675532466
mlopez <monicalopezoliveros@gmail.com>
2024-11-15 17:01:17 -0600

    Configured ActiveAdmin: To upload images.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,18 +1,19 @@
 ActiveAdmin.register Product do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :price, :category_id, :stock, :status_flag
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :price, :category_id, :stock, :status_flag]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :name, :price, :category_id, :stock, :status_flag, :image
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :price
+      f.input :category_id
+      f.input :stock
+      f.input :status_flag
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+
+
 end
+
